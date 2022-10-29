055f827d8adc6bd7dfa038734bf829a469ce1632
hj <hyeji4775@naver.com>
2022-10-30 02:40:07 +0900

    위험지역 코드 임시 삭제

--- capFront/App.js
+++ capFront/App.js
@@ -18,15 +18,7 @@ async function requestPermission() {
 }
 
 function App() {
-  const [dangerAreas, setDangerAreas] = useState([]);
-  const componentDidMount = async () => {
-    //guGun을 바꿔가면서 여러 번 호출 필요
-    const response = await fetch('http://taas.koroad.or.kr/data/rest/frequentzone/pedstrians?authKey=%2Buvw8mtEC8SxmORSnCRCjrnB2BDLitKPgiqG4575Ym%2Btu2WpCtx3C25RGKSVuxEb&searchYearCd=2022032&siDo=11&guGun=680&type=json');
-    const danger = await response.json();
-    setDangerAreas(danger.items.item);
-    console.log(dangerAreas);
-  }
-
+ 
   useEffect(() => {
     componentDidMount();
   }, []);
@@ -88,27 +80,7 @@ function App() {
           />
 
           <Polyline coordinate={{latitude: latitude, longitude: longitude}} strokeColor="#CAEF53"/> 
-        
-        {dangerAreas.length === 0 ? (
-            <View>
-              <ActivityIndicator
-                color="white"
-                style={{margin: 10}}
-                size="large"
-              />
-            </View>
-          ) : (
-            dangerAreas.map((dangerArea, index) => {
-              return (
-                //마커 띄우긴 가능 그러나 geom_json이 string으로 읽혀 polyline으로 출력 어려움
-                <Marker
-                  key={index}
-                  coordinate={{latitude: parseFloat(dangerArea.la_crd), longitude: parseFloat(dangerArea.lo_crd)}}
-                />
-              )
-            })
-          )}
-
+      
         </MapView>
       </View>
   );
