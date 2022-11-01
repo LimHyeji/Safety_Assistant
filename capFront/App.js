16570913b51cbf14152f35da053d5303b3f4f70f
hj <hyeji4775@naver.com>
2022-11-02 00:13:42 +0900

    타이머기능 구현중

--- capFront/App.js
+++ capFront/App.js
@@ -17,8 +17,42 @@ async function requestPermission() {
   }
 }
 
-function App() {
 
+function App() {
+  /*
+//timer 컴포넌트 생성
+const Timer=({mm,ss})=>{
+  const [minutes, setMinutes]=useState(parseInt(mm));
+  const [seconds,setSeconds]=useState(parseInt(ss));
+
+  
+useEffect(() => {
+  const countdown = setInterval(() => {
+    if (parseInt(seconds) > 0) {
+      setSeconds(parseInt(seconds) - 1);
+    }
+    if (parseInt(seconds) === 0) {
+      if (parseInt(minutes) === 0) {
+          clearInterval(countdown);
+      } else {
+        setMinutes(parseInt(minutes) - 1);
+        setSeconds(59);
+      }
+    }
+  }, 1000);
+  return () => clearInterval(countdown);
+}, [minutes, seconds]);
+
+
+
+  return(
+    <div>
+      {minutes}:{seconds<10?'0${seconds}':seconds}
+    </div>
+  );
+
+};
+  */
   const [latitude, setLatitude] = useState(null)
   const [longitude, setLongitude] = useState(null)
   const [route, setRoute] = useState([]);
@@ -81,6 +115,8 @@ function App() {
         </MapView>
       </View>
   );
+
+
 }
 
 export default App;
