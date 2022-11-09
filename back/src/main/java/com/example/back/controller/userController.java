29efd54c33e0eae1be0b5d6ee50067fd22a4de25
SSiik <sik_5571@naver.com>
2022-11-09 15:35:02 +0900

    백엔드 비즈니스 로직 추가 (회원가입,로그인,위치 주고받기)

--- back/src/main/java/com/example/back/controller/userController.java
+++ back/src/main/java/com/example/back/controller/userController.java
@@ -1,30 +1,28 @@
 package com.example.back.controller;
 
-import com.example.back.Domain.Dto.LoginRequest;
-import com.example.back.Domain.Dto.ResponseDto;
-import com.example.back.Domain.Dto.signDto;
+import com.example.back.Domain.Dto.*;
 import com.example.back.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class userController {
 
     private final UserService userService;
 
     @PostMapping("/user/signup")
-    public void execSignup(@Validated @RequestBody signDto signDto) {
-//        try {
+    public jsonResponse execSignup(@Validated @RequestBody signDto signDto) {
+            jsonResponse jsonReponse = new jsonResponse();
+            jsonReponse.setMsg("It is Check to communicate");
+            log.info(signDto.getUserId()+"!!!!!!!!!!!!!!!!!!!!!");
+            log.info(signDto.getPassword()+"!!!!!!!!!!!!!!!!!!!!!");
             userService.joinUser(signDto);
-//        } catch(IllegalStateException e){
-//            String errMsg = e.getMessage();
-//            model.addAttribute("errMsg",errMsg);
-//            return "/ShowErr";
-//        }
-        //일단 test용이라서 void로 진행한다.
+            return jsonReponse;
     }
 
 
@@ -33,6 +31,12 @@ public class userController {
         ResponseDto responseDto = userService.loginChk(loginRequest.getUserId(), loginRequest.getPassword());
         return responseDto;
         // 로그인 상태유지와 더불어서 token으로 이제 API를 사용할수가 있습니다.
+    }
+
+    @PostMapping("/user/child")
+    public childrenDto req(@RequestBody childrenDto childrenDto) {
+        childrenDto dto1 = userService.childrenRequest(childrenDto);
+        return dto1;
     }
 
 
