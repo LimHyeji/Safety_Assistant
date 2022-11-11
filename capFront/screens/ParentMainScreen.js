34b0a25182d1d2d1274f22d244b2e8b1ff8f3135
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2022-11-12 01:35:31 +0900

    부모 메인 화면 수정
    경로 표시 -> 새로고침 버튼으로 수정
    알림 버튼 추가(아직 기능 X)
    자녀 위경도 받아보고 코드 수정 필요

--- capFront/screens/ParentMainScreen.js
+++ capFront/screens/ParentMainScreen.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet, Platform, Alert, PermissionsAndroid, ActivityIndicator, TouchableOpacity} from "react-native";
-import Geolocation from "react-native-geolocation-service";
 import MapView, {Marker, Polyline, Circle} from "react-native-maps";
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
@@ -33,42 +32,25 @@ function ParentMain({navigation}) {
       }
   }
 
+  /*
   const trackPosition = () => {
-    requestPermission().then(result => {
+    requestPermission().then(async(result) => {
       console.log({result});
       if(result === "granted") {
-        const _watchId = Geolocation.watchPosition(
-          position => {
-            const {latitude, longitude} = position.coords;
-            setLatitude(latitude);
-            setLongitude(longitude);
-            setRoute(route => [...route, {latitude: latitude, longitude: longitude}]);
-          },
-          error => {
-            console.log(error);
-          },
-          {
-            enableHighAccuracy: true,
-            distanceFilter: 0,
-            interval: 3000,
-            fastestInterval: 2000,
-          },
-        );
-    
-        return () => {
-          if(_watchId) {
-            Geolocation.clearWatch(_watchId);
-          }
-        }
+        const childLocation = await fetch('http://34.64.74.7:8081/user/login/parent');
+        console.log(childLocation);
+        setLatitude(childLocation.latitude);
+        setLongitude(childLocation.latitude);
       }
     });
-  }
-  const showChildRoute = async() => {
-    /*
-    const childRoute = await fetch('', {method: 'GET'});
-    console.log(childRoute);
-    setRoute(childRoute);*/
-    setShow(true);
+  }*/
+  const showChildLocation = async() => {
+    /*const childLocation = await fetch('http://34.64.74.7:8081/user/login/parent');
+    console.log(childLocation);
+    setLatitude(childLocation.latitude);
+    setLongitude(childLocation.latitude);
+    setShow(true);*/
+    console.log("touch");
   }
 
   const showInfo = () => {
@@ -84,7 +66,9 @@ function ParentMain({navigation}) {
 
   useEffect(() => {
     componentDidMount();
-    trackPosition();
+    setLatitude(37); // 임시
+    setLongitude(127); 
+    //trackPosition();
   }, []);
   
   if(!latitude && !longitude) {
@@ -97,6 +81,11 @@ function ParentMain({navigation}) {
 
   return (
       <View style={{ flex: 1 }}>
+        <View>
+          <TouchableOpacity style={styles.alarmButton} onPress={() => showChildLocation()}>
+            <Icon name="bell" size={25} color={"#000"}/>
+          </TouchableOpacity>
+        </View>
         <MapView
           style={{ flex: 1, width:'100%', height:'100%' }}
           initialRegion={{
@@ -110,21 +99,6 @@ function ParentMain({navigation}) {
         <Marker
           coordinate={{latitude: latitude, longitude: longitude}}
         />
-
-        {show === true ? (
-          <Polyline
-            coordinates={route}
-            strokeColor="#000"
-            strokeColors={['#7F0000']}
-            strokeWidth={5}
-            tappable={true}
-            onPress={() => showInfo()}
-          />
-        ) : (
-          <></>
-        )
-        
-        }
 
         {dangerAreas.length === 0 ? (
               <ActivityIndicator
@@ -153,8 +127,8 @@ function ParentMain({navigation}) {
           
         </MapView>
         <View>
-          <TouchableOpacity style={styles.routeButton} onPress={() => showChildRoute()}>
-            <Icon name="route" size={30} color={"#000"}/>
+          <TouchableOpacity style={styles.reloadButton} onPress={() => showChildLocation()}>
+            <Icon name="redo-alt" size={30} color={"#000"}/>
           </TouchableOpacity>
         </View>
         <View>
@@ -167,7 +141,7 @@ function ParentMain({navigation}) {
 export default ParentMain;
 
 const styles = StyleSheet.create({
-  routeButton: {
+  reloadButton: {
     backgroundColor: "#CAEF53",
     alignItems: "center",
     justifyContent: 'center',
@@ -188,5 +162,16 @@ const styles = StyleSheet.create({
         elevation: 2,
       }
     })
-  }
+  },
+  alarmButton: {
+    alignItems: "center",
+    justifyContent: 'center',
+    width: 40,
+    height: 40,
+    borderRadius: 35,
+    position: "absolute",
+    top: 10,
+    right: 10,
+    zIndex: 1,
+  },
 })
