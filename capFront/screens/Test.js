10bf1d4b9ed265704e5c06b991d6a852422c2791
hj <ihj028003@gmail.com>
2022-11-21 23:55:49 +0900

    우편번호

--- capFront/screens/Test.js
+++ capFront/screens/Test.js
@@ -1,45 +1,10 @@
 import React, { useState } from 'react';
 import { View,} from 'react-native';
 import Postcode from '@actbase/react-daum-postcode';
-/*
-const PostCodeStyle = {
-    display: "block",
-    position: "absolute",
-    top: "10%",
-    width: "60%",
-    height: "60%",
-    padding: "7px",
-  };
-
-function HandlePostCode(data) {
-    let fullAddress = data.address;
-    let extraAddress = ''; 
-    
-    if (data.addressType === 'R') {
-      if (data.bname !== '') {
-        extraAddress += data.bname;
-      }
-      if (data.buildingName !== '') {
-        extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
-      }
-      fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
-    }
-    console.log(data)
-    console.log(fullAddress)
-    console.log(data.zonecode)
-
-return(
-    <View>
-        <Postcode style={{flex:1, width:'100%', zIndex:999}} onComplete={HandlePostCode} />
-    </View>
-);
-};
- 
-export default HandlePostCode;
-*/
-//*
+import { Button } from 'react-native-paper';
 
 function app({navigation}){
+  /*
 const getAddressData = data => {
  let defaultAddress = ''; // 기본주소
  if (data.buildingName === 'N') {
@@ -60,6 +25,17 @@ return(
   style={{ flex: 1, width: '100%', zIndex: 999 }}
   jsOptions={{ animation: true }}
   onSelected={data => getAddressData(data)}
+  />
+  </View>
+);
+*/
+
+return(
+  <View>
+    <Postcode
+    style={{ width: 320, height: 320 }}
+    jsOptions={{ animation: true }}
+    onSelected={data => alert(JSON.stringify(data))}
   />
   </View>
 );
