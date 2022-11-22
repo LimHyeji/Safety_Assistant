6c597b5f34257aeebbfeb5bf651719a50fec3053
hj <ihj028003@gmail.com>
2022-11-23 02:46:37 +0900

    우편번호 데이터 async로 넘기기 구현중

--- capFront/screens/addressScreen.js
+++ capFront/screens/addressScreen.js
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { View,} from 'react-native';
 import Postcode from '@actbase/react-daum-postcode';
 import Geocode from "react-geocode";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-function toLatLng(address){
+const storeLatLng=async(lat,lng,{navigation})=>{
+  await AsyncStorage.setItem(
+    'gpsdata',
+    JSON.stringify({
+      lat:lat,
+      lng:lng
+    })
+  )
+};
+
+function toLatLng(address,{navigation}){
   Geocode.setApiKey("AIzaSyD3wawfdvi_QBp0XYbXPC47nXWUUEVX4wY");
   Geocode.setLanguage("en");
   Geocode.setRegion("es");
@@ -13,6 +24,10 @@ function toLatLng(address){
       console.log(address);
       const { lat, lng } = response.results[0].geometry.location;
       console.log(lat, lng);
+    
+      storeLatLng(lat,lng,{navigation});
+      navigation.navigate("Registerpage");
+
     },
     (error) => {
       console.error(error);
@@ -20,15 +35,15 @@ function toLatLng(address){
   );
 }
 function app({navigation}){
-return(
-  <View>
-    <Postcode
-    style={{ width: 320, height: 320 }}
-    jsOptions={{ animation: true }}
-    onSelected={data => toLatLng(data.addressEnglish)}
-  />
-  </View>
-);
+  return(
+    <View>
+      <Postcode
+      style={{ width: 320, height: 320 }}
+      jsOptions={{ animation: true }}
+      onSelected={data => toLatLng(data.addressEnglish,{navigation})}
+    />
+    </View>
+  );
 }
 export default app;
 //*/
