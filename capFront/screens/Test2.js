95194a8219f17c6cdcea19405425acd0f456cd2f
hj <ihj028003@gmail.com>
2022-11-23 00:04:42 +0900

    팝업 구현 진행중

--- capFront/screens/Test2.js
+++ capFront/screens/Test2.js
@@ -1,4 +1,49 @@
 import React, { useState, useEffect } from 'react';
+import { View,Button,} from 'react-native';
+import Postcode from '@actbase/react-daum-postcode';
+import Geocode from "react-geocode";
+import Modal from 'react-native-modal'
+
+function toLatLng(address){
+  Geocode.setApiKey("AIzaSyD3wawfdvi_QBp0XYbXPC47nXWUUEVX4wY");
+  Geocode.setLanguage("en");
+  Geocode.setRegion("es");
+  Geocode.setLocationType("ROOFTOP");
+  Geocode.fromAddress(address).then(
+    (response) => {
+      console.log(address);
+      const { lat, lng } = response.results[0].geometry.location;
+      console.log(lat, lng);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+}
+function app({navigation}){
+  const [isModal, setModal] = useState(false);
+return(
+    <View>
+      <Modal isVisible={isModal}>
+        <View>
+        <Postcode
+          style={{ width: 320, height: 320 }}
+          jsOptions={{ animation: true }}
+          onSelected={data => {
+            alert(JSON.stringify(data));
+            setModal(false);
+          }}
+        />
+        </View>
+      </Modal>
+      <Button title="주소찾기" onClick={() => setModal(true)}></Button>
+    </View>
+);
+}
+export default app;
+
+/*
+import React, { useState, useEffect } from 'react';
 import { Text,} from 'react-native';
 
 function app({mm,ss},{navigation}){
