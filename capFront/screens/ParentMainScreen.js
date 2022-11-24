85e2afd4b240c331b85fc32b1e3a461c4986c8ef
hj <ihj028003@gmail.com>
2022-11-25 01:11:20 +0900

    부모 마커

--- capFront/screens/ParentMainScreen.js
+++ capFront/screens/ParentMainScreen.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet, Platform, Alert, PermissionsAndroid, ActivityIndicator, TouchableOpacity} from "react-native";
-import MapView, {Marker, Polyline, Circle} from "react-native-maps";
+import MapView, {Marker, Polyline} from "react-native-maps";
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Geolocation from "react-native-geolocation-service";
 
 //위치 접근 권한 받기
 async function requestPermission() {
@@ -18,50 +19,52 @@ async function requestPermission() {
 
 function ParentMain({navigation}) {
 
-  const [latitude, setLatitude] = useState(null)
+  const [latitude, setLatitude] = useState(null); //부모의 위치데이터
   const [longitude, setLongitude] = useState(null);
-  const [route, setRoute] = useState([]); // 이동 경로
+
+  const [childLat, setChildLat] = useState(null); //자녀의 위치데이터
+  const [childLng, setChildLng] = useState(null);
+  const [route, setRoute] = useState([]); // 자녀의 이동 경로
   const [show, setShow] = useState(false);
-  const [dangerAreas, setDangerAreas] = useState([]); // 위험 지역
   
-  const trackPosition = async() => {
+  const trackPosition = () => {  //부모의 위치추적
     requestPermission();
     try{
-      //const value = await AsyncStorage.getItem('userData');
-      //const parsevalue = JSON.parse(value);
-      fetch('http://34.64.74.7:8081/user/login/parent', {
-        method: "POST",
-        body: JSON.stringify({
-          //userId: parsevalue.childrenInfo[0].userId,
-          "userId": "child",
-          "idx": true,
-        }),
-        headers : {'Content-Type' : 'application/json; charset=utf-8'}
-      })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson);
-        setLatitude(parseFloat(responseJson.latitude));
-        setLongitude(parseFloat(responseJson.longitude));
-        setRoute(route => [...route, {latitude: latitude, longitude: longitude}]);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      const _watchId = Geolocation.watchPosition(
+        position => {
+          const {latitude, longitude} = position.coords;
+          setLatitude(latitude);
+          setLongitude(longitude);
+        },
+        error => {
+          console.log(error);
+        },
+        {
+          enableHighAccuracy: true,
+          distanceFilter: 0,
+          interval: 3000,
+          fastestInterval: 2000,
+        },
+      );
+      return () => {
+        if(_watchId) {
+          Geolocation.clearWatch(_watchId);
+        }
+      }
     }catch(error){
       console.log(error);
     }
   }
 
-  const showChildLocation = async() => {
+  const showChildLocation = async() => {  //자녀의 위치추적
     try{
-      //const value = await AsyncStorage.getItem('userData');
-      //const parsevalue = JSON.parse(value);
+      //const value = await AsyncStorage.getItem('userData'); //테스트를 위한 주석처리
+      //const parsevalue = JSON.parse(value); //테스트를 위한 주석처리
       fetch('http://34.64.74.7:8081/user/login/parent', {
         method: "POST",
         body: JSON.stringify({
-          //userId: parsevalue.childrenInfo[0].userId,
-          "userId": "child",
+          //userId: parsevalue.childrenInfo[0].userId, //테스트를 위한 주석처리
+          "userId": "child",  //테스트를 위한 임시값
           "idx": true,
         }),
         headers : {'Content-Type' : 'application/json; charset=utf-8'}
@@ -69,9 +72,9 @@ function ParentMain({navigation}) {
       .then((response) => response.json())
       .then((responseJson) => {
         console.log(responseJson);
-        setLatitude(parseFloat(responseJson.latitude));
-        setLongitude(parseFloat(responseJson.longitude));
-        setRoute(route => [...route, {latitude: latitude, longitude: longitude}]);
+
+        if(responseJson.latitude!==null){
+        setChildLat(parseFloat(responseJson.latitude));
         setChildLng(parseFloat(responseJson.longitude));
         setRoute(route => [...route, {latitude:childLat, longitude: childLng}]);
         setShow(true);
