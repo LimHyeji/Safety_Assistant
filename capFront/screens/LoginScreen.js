6ae32688c70d999c661a79d45d6f4883a21fc121
hj <ihj028003@gmail.com>
2022-11-26 00:32:38 +0900

    Revert "회원가입 완성"
    This reverts commit 369f1461baeb3e107da1fe1e67a46c328a8ef0ce.

--- capFront/screens/LoginScreen.js
+++ capFront/screens/LoginScreen.js
@@ -105,7 +105,7 @@ return (
       )
       navigation.navigate('ParentMainpage');
     }
-    if(responseJson.idx===false){ //자녀일 경우의 저장 내용
+    else{ //자녀일 경우의 저장 내용
       await AsyncStorage.setItem(
         'userData',
         JSON.stringify({
