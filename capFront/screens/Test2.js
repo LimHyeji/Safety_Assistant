babb67f518ca1fe74535cf5366c202de243ea7d4
hj <ihj028003@gmail.com>
2022-12-06 03:18:36 +0900

    test 페이지 타이머부분 지움

--- capFront/screens/Test2.js
+++ capFront/screens/Test2.js
@@ -1,36 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Text,Linking,Button} from 'react-native';
 import GoogleFit, { Scopes, BucketUnit } from 'react-native-google-fit'
-/*
-function app({mm,ss},{navigation}){
-    const [minutes,setMinutes]=useState(parseInt(mm));
-    const [seconds,setSeconds]=useState(parseInt(ss));
-
-    useEffect(() => {
-        const countdown = setInterval(() => {
-          if (parseInt(seconds) > 0) {
-            setSeconds(parseInt(seconds) - 1);
-          }
-          if (parseInt(seconds) === 0) {
-            if (parseInt(minutes) === 0) {
-                clearInterval(countdown);
-            } else {
-              setMinutes(parseInt(minutes) - 1);
-              setSeconds(59);
-            }
-          }
-        }, 1000);
-        return () => clearInterval(countdown);
-      }, [5, 0]);
-
-
-    return(
-        <Text>
-            {minutes}:{seconds < 10 ? '0${seconds}':seconds}
-        </Text>
-    );
-};
-*/0
 
 /*
 function app(){
